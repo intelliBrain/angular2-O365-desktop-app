c5566f9872567c486450d60782512c69a2884401
cecildt <cecildt@gmail.com>
2017-01-06 07:09:17 +0100

    Updated packages and added eslint config.

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
-const electron = require('electron');
+const electron = require("electron");
 const app = electron.app;
 
 const AdalMainConfig = require("./main-config");
 
-require('electron-debug')({ showDevTools: true });
+require("electron-debug")({ showDevTools: true });
 
 // browser-window creates a native window
 const BrowserWindow = electron.BrowserWindow;
@@ -13,43 +13,42 @@ let mainWindow = null;
 let hash = "";
 
 // Allows for live-reload while developing the app
-require('electron-reload')(__dirname + '/build');
+require("electron-reload")(__dirname + "/build");
 
 function createWindow() {
 
   // Initialize the window to our specified dimensions
   mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 900,
     autoHideMenuBar: true,
+    height: 900,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
+    width: 1200,
   });
 
   mainWindow.maximize();
   // Tell Electron where to load the entry point from
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL("file://" + __dirname + "/index.html");
 
   // Clear out the main window when the app is closed
-  mainWindow.on('closed', function () {
+  mainWindow.on("closed", function () {
     mainWindow = null;
   });
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', function () {
-  if (process.platform != 'darwin') {
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', function () {
+app.on("activate", function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
   }
 });
-
